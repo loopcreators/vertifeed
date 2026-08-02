18185c7afd3bcac568993e98e269d8cabdae9ac8
Arun Pariyar <openarungeek@gmail.com>
2026-08-02 14:38:34 +0545

    update plugin name

--- vertifeed.php
+++ vertifeed.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name:       VertiFeed
+ * Plugin Name:       VertiFeed - Vertical Media Gallery
  * Plugin URI:        https://github.com/loopcreators/vertifeed
  * Description:       Play Media Library videos in a vertical swipe gallery via Gutenberg blocks.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 6.5
  * Requires PHP:      7.4
  * Tested up to:      7.0
@@ -17,7 +17,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'VERTIFEED_VERSION', '1.0.0' );
+define( 'VERTIFEED_VERSION', '1.0.1' );
 define( 'VERTIFEED_PATH', plugin_dir_path( __FILE__ ) );
 define( 'VERTIFEED_URL', plugin_dir_url( __FILE__ ) );
 
